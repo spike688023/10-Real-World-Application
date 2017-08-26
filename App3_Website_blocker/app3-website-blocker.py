5fa8831cfb4ce83423a2471bd335b662d9333c43
abliarsec <jack_6880hotmail.com>
2017-08-26 17:59:58 +0800

    Run App3

--- App3_Website_blocker/app3-website-blocker.py
+++ App3_Website_blocker/app3-website-blocker.py
@@ -4,27 +4,27 @@ from datetime import datetime as dt
 hosts_temp=r"/Users/nickboy/Udemy/10-Real-World-Application/App3_Website_blocker/hosts"
 hosts_path="/etc/hosts"
 redirect="127.0.0.1"
-#website_list=["www.facebook.com","facebook.com","dub119.mail.live.com","www.dub119.mail.live.com"]
-website_list=["www.facebook.com","www.dub119.mail.live.com"]
+website_list=["www.facebook.com","facebook.com","dub119.mail.live.com","www.dub119.mail.live.com"]
+#website_list=["www.facebook.com","www.dub119.mail.live.com"]
 
 while True:
-    if dt(dt.now().year,dt.now().month,dt.now().day,12) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
+    if dt(dt.now().year,dt.now().month,dt.now().day,12) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,18):
         print("Working hours...")
-        with open(hosts_temp,'r+') as file:
+        with open(hosts_path,'r+') as file:
             content=file.read()
             for website in website_list:
                 if website in content:
                     pass
                 else:
-                    file.write(redirect+" "+ website+"\n")
+                    file.write(redirect+" "+website+"\n")
     else:
-        with open(hosts_temp,'r+') as file:
+        with open(hosts_path,'r+') as file:
             content=file.readlines()
             file.seek(0)
             for line in content:
                 if not any(website in line for website in website_list):
-                    print(line)
-                    print(any(website in line for website in website_list))
+                    #print(line)
+                    #print(any(website in line for website in website_list))
                     file.write(line)
             file.truncate()
         print("Fun hours...")
