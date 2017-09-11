d419ed8c2be3d0617d321d81b17ca5df03022c1b
Spike2_Lin <Spike2_Lin@asus.com>
2017-09-11 17:40:19 +0800

    I know how 6.to-do-list.py works

--- Implement-6-Small-APP/6.to-do-list.py
+++ Implement-6-Small-APP/6.to-do-list.py
@@ -3,6 +3,7 @@ class ToDoList:
         self.file_name = file_name
         self.tasks = self.load_file_into_list()
 
+    # 這裡拿一個檔案紀録, 要做的事有那些.
     def load_file_into_list(self):
         tasks = []
         with open(self.file_name, 'r') as file:
@@ -14,6 +15,9 @@ class ToDoList:
         for index, task in enumerate(self.tasks, start=1):
             print('{}) {}'.format(index, task))
             
+    # 這裡感覺,  寫的不好, 它參數放 w , 每次寫入會清空, 再重頭寫哎.
+    # 案, 它會把檔內的資料, 先全部讀出來, 之後有update的時侯, 
+    # 再全部寫回去.
     def write_into_file(self):
         with open(self.file_name, 'w') as file:
             for task in self.tasks:
@@ -23,6 +27,8 @@ class ToDoList:
         self.tasks.append(task)
         self.write_into_file()
 
+    # 還做了一個判庍, 去看 使用者輸入的index,有沒有在裡面.
+    # 如果沒有, 就重頭再跑一次.
     def done_task(self, task_index):
         task_exist = False
         for index, task in enumerate(self.tasks, start=1):
@@ -39,10 +45,13 @@ def todo_help():
     print('* Create new task: [todo TASK]') #todo do homework
     print('* Mark a task as done: [done INDEX]') # ['todo', 'do homework']
     print('See the to-do list: [list]\n') # done 2 # done 5
+    print('See help:[help]\n') # done 2 # done 5
+    print('Get out: [exit]\n') # done 2 # done 5
 
 def run():
     todo_help()
     while True:
+        # 這 class , 主要的東西, 就是餵一個檔案進去.
         todolist = ToDoList('todolist.txt')
         cmd_detail = input('Enter cmd: ')
         cmd = cmd_detail.split(' ', 1)[0]
@@ -60,3 +69,21 @@ def run():
             break
         
 run()
+
+"""
+第二個參數, 代表只切一個出來.
+split(...)
+    S.split([sep[, maxsplit]]) -> list of strings
+
+    Return a list of the words in S, using sep as the
+    delimiter string.  If maxsplit is given, at most maxsplit
+    splits are done. If sep is not specified or is None, any
+    whitespace string is a separator and empty strings are
+    removed from the result.
+
+>>> "aaaa bbb ccc ddd".split(' ')
+['aaaa', 'bbb', 'ccc', 'ddd']
+>>> "aaaa bbb ccc ddd".split(' ',1)
+['aaaa', 'bbb ccc ddd']
+
+"""
