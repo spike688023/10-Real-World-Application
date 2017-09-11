76170bba170298114fd0a6816adfe8009e1e13dd
abliarsec <jack_6880hotmail.com>
2017-09-11 23:20:20 +0800

    Add some comments

--- App4-flask-website/script1.py
+++ App4-flask-website/script1.py
@@ -12,3 +12,34 @@ def about():
 
 if __name__=="__main__":
     app.run(debug=True)
+
+"""
+The Django template language
+原來 html 中的 {} 是變數，
+
+這個整體 的html 叫， template language.
+
+In this example, the {% block %} tags define four blocks that child templates can fill in. All the block tag does is tell the template engine that a child template may override those portions of the template.
+
+Reference:
+http://flask.pocoo.org/docs/0.12/patterns/templateinheritance/
+
+這是把出一個獨立的環境
+python -m venv virtual
+
+-m 是把後 面的module 當成執行檔在執行 ， 後面的xxx ，是指令，
+
+對於那個module 要下的指令, 像這個範例，就 是在當目録下，
+
+拉出一個獨立的環境，東西放在virtual , 像你一班執 行 的python  pip ,
+
+都會被放到這裡，  如果想在這獨 立的環境安裝 東西， 執 行 pip ，
+
+也要用這個folder裡面的才行, 像virtual\scripts\python.
+
+
+難怪之前有一個指令是  python -m flask run
+
+t 
+
+"""
--- Implement-6-Small-APP/5.witch-hunter.py
+++ Implement-6-Small-APP/5.witch-hunter.py
@@ -55,7 +55,7 @@ class Hunter:
         else:
             print('{} ran away!'.format(witch.name))
             return False
-    
+
 
 def start_game():
     hunter = Hunter('hunter', 1)
@@ -78,4 +78,52 @@ def start_game():
             print('Hunter stop tracing')
 
 start_game()
-    
+
+"""
+遇到女巫， 是計算當下我的功擊力跟女巫的攻擊力，
+
+誰大就誰win， 我還多一個爆集參數， 但50％，
+
+就 True False而已。
+
+功擊計算，是用隨機變數值 成上自身等級。
+
+火巫會不會發 火球  也是50%.  發火球 也只是
+
+攻擊力成二倍。
+
+attack()  會傳入 女巫的物件，以及 是否爆集。
+
+witch 是隨機從三個中， 選一個出來。
+
+這就 一直打女巫，完全不會停。
+"""
+"""
+class 內的self , 有篇文章寫的很好，
+
+这里先理解下函数参数里面的self和cls.这个self和cls是对类或者实例的绑定,对于一般的函数来说我们可以这么调用foo(x),这个函数就是最常用的,它的工作跟任何东西(类,实例)无关.对于实例方法,我们知道在类里每次定义方法的时候都需要绑定这个实例,就是foo(self, x),为什么要这么做呢?因为实例方法的调用离不开实例,我们需要把实例自己传给函数,调用的时候是这样的a.foo(x)(其实是foo(a, x))
+
+Reference:
+https://github.com/taizilongxu/interview_python#10-args-and-kwargs
+
+
+>>> from datetime import datetime
+>>> now = datetime.now()
+>>> print(str(now))
+2017-04-22 15:41:33.012917
+>>> print(repr(now))
+datetime.datetime(2017, 4, 22, 15, 41, 33, 12917)
+通过 str() 的输出结果我们能很好地知道 now 实例的内容，但是却丢失了 now 实例的数据类型信息。而通过 repr() 的输出结果我们不仅能获得 now 实例的内容，还能知道 now 是 datetime.datetime 对象的实例。
+
+因此 str() 与 repr() 的不同在于：
+
+str() 的输出追求可读性，输出格式要便于理解，适合用于输出内容到用户终端。
+repr() 的输出追求明确性，除了对象内容，还需要展示出对象的数据类型信息，适合开发和调试阶段使用。
+
+>>> datetime.__repr__
+<slot wrapper '__repr__' of 'datetime.datetime' objects>
+
+Reference:
+http://www.jianshu.com/p/2a41315ca47e
+
+"""
